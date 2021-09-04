f43954b0fdd1e9ca6345b1b5dda4831dd6bc92fe
maxdup <mdupuis@hotmail.ca>
2021-09-03 23:12:19 -0400

    overhaul of transclusion system

--- src/angular/ngVueLinker.js
+++ src/angular/ngVueLinker.js
@@ -12,8 +12,8 @@ import watchSpecialAttributes from '../components/props/watchSpecialAttributes'
 export function ngVueLinker (componentName, jqElement, elAttributes, scope, $injector) {
   if (!jqElement.parent().length) throw new Error('ngVue components must have a parent tag or they will not render')
 
+  const $compile = $injector.get('$compile')
   const $ngVue = $injector.has('$ngVue') ? $injector.get('$ngVue') : null
-
   const dataExprsMap = getPropExprs(elAttributes)
   const Component = getVueComponent(componentName, $injector)
   const directives = evaluateDirectives(elAttributes, scope) || []
@@ -31,20 +31,68 @@ export function ngVueLinker (componentName, jqElement, elAttributes, scope, $inj
 
   const mounted = rootProps.mounted
   const props = Object.assign({}, rootProps)
-  props.mounted = function () {
-    const element = jqElement[0]
-    if (element.innerHTML.trim()) {
-      let html
-      if (element.children.length === 0) {
-        const span = document.createElement('span')
-        span.innerHTML = element.innerHTML.trim()
-        html = span
-      } else {
-        html = element.children[0]
+
+  let namedTemplates = {}
+  const element = jqElement[0]
+  if (element.innerHTML.trim()) {
+    if (element.children.length === 0) {
+      // todo: revise
+      const span = document.createElement('span')
+      span.innerHTML = element.innerHTML.trim()
+    } else {
+      let templates = []
+      let unnamedTemplates = []
+      let notTemplates = []
+
+      element.children.forEach(function (el) {
+        if (el.tagName === 'TEMPLATE') {
+          templates.push(el)
+        } else {
+          notTemplates.push(el)
+        }
+      })
+
+      templates.forEach(el => {
+        let named = null
+        el.attributes.forEach(attr => {
+          if (attr.name.startsWith('v-slot:')) {
+            named = attr.name.split(':')[1]
+          }
+        })
+        if (named) {
+          // namedTemplates[named] = el.cloneNode(true).content.firstElementChild.outerHTML;
+          // let div = document.createElement('div')
+          // div.appendChild(el.cloneNode(true).content)
+          // namedTemplates[named] = div
+          namedTemplates[named] = el.cloneNode(true).content
+        } else {
+          // unnamedTemplates.push(el.cloneNode(true).content.firstElementChild.outerHTML);
+          unnamedTemplates.push(el.cloneNode(true).content)
+        }
+      })
+      if (!namedTemplates['default']) {
+        if (unnamedTemplates.length > 0) {
+          namedTemplates['default'] = unnamedTemplates[0]
+        } else {
+          if (notTemplates.length > 0) {
+            namedTemplates['default'] = notTemplates[0]
+          } else {
+            namedTemplates['default'] = element.innerHTML
+          }
+        }
       }
-      const slot = this.$refs.__slot__
-      slot.parentNode.replaceChild(html, slot)
     }
+  }
+  let slotwrapper = Vue.component('slotwrapper', {
+    template: '<div></div>',
+    props: ['rawhtml'],
+    mounted () {
+      $compile(this.rawhtml)(scope)
+      this.$el.parentNode.replaceChild(this.rawhtml, this.$el)
+    }
+  })
+
+  props.mounted = () => {
     if (angular.isFunction(mounted)) {
       mounted.apply(this, arguments)
     }
@@ -63,10 +111,16 @@ export function ngVueLinker (componentName, jqElement, elAttributes, scope, $inj
     el: jqElement[0],
     data: reactiveData,
     render (h) {
+      let scopedslots = {}
+      Object.keys(namedTemplates).forEach(key => {
+        scopedslots[key] = () => {
+          return h(slotwrapper, { props: { rawhtml: namedTemplates[key] } })
+        }
+      })
       return (
         <Component
           {...{ directives }}
-          {...{ props: reactiveData._v.props, on, attrs: reactiveData._v.attrs }}
+          {...{ props: reactiveData._v.props, on, scopedSlots: scopedslots, attrs: reactiveData._v.attrs }}
           {...reactiveData._v.special}
         >
           {<span ref="__slot__" />}
